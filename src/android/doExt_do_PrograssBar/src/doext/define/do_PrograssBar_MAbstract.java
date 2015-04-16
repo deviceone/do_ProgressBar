cfdc99300978d0441591c375c4ecfb8b205ed924
maorui <maoruiily@163.com>
2015-04-16 19:50:42 +0800

    添加 PrograssBar

---
+++ src/android/doExt_do_PrograssBar/src/doext/define/do_PrograssBar_MAbstract.java
@@ -0,0 +1,26 @@
+package doext.define;
+
+import core.object.DoUIModule;
+import core.object.DoProperty;
+import core.object.DoProperty.PropertyDataType;
+
+
+public abstract class do_PrograssBar_MAbstract extends DoUIModule{
+
+	protected do_PrograssBar_MAbstract() throws Exception {
+		super();
+	}
+	
+	/**
+	 * 初始化
+	 */
+	@Override
+	public void onInit() throws Exception{
+        super.onInit();
+        //注册属性
+		this.registProperty(new DoProperty("max", PropertyDataType.Number, "100", false));
+		this.registProperty(new DoProperty("progress", PropertyDataType.Number, "", false));
+		this.registProperty(new DoProperty("secondaryProgress", PropertyDataType.Number, "", false));
+		this.registProperty(new DoProperty("style", PropertyDataType.String, "normal", true));
+	}
+}
---
+++ src/android/doExt_do_PrograssBar/src/doext/implement/do_PrograssBar_Model.java
@@ -0,0 +1,15 @@
+package doext.implement;
+
+import doext.define.do_PrograssBar_MAbstract;
+
+/**
+ * 自定义扩展组件Model实现，继承do_PrograssBar_MAbstract抽象类；
+ *
+ */
+public class do_PrograssBar_Model extends do_PrograssBar_MAbstract {
+
+	public do_PrograssBar_Model() throws Exception {
+		super();
+	}
+	
+}
